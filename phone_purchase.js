a463cd33562e245488f06ad581cdc9d4484374f0
Paul Stivers <paul.stivers@fidelitylife.com>
2017-07-06 11:36:03 -0700

    first commit

--- phone_purchase.js
+++ phone_purchase.js
@@ -2,7 +2,8 @@ var TAX_RATE = 0.08;
 var PHONE_PRICE = 500.00;
 var ACCESSORY_PRICE = 30.00;
 var SPENDING_THRESHOLD = 800.00;
-var STARTING_BANK_BALANCE = 3000.00;
+var STARTING_BANK_BALANCE = prompt('what is your staring bank balance? ');
+// var STARTING_BANK_BALANCE = 3000.00;
 var total;
 var bank_balance = STARTING_BANK_BALANCE;
 
@@ -11,22 +12,22 @@ function calculateTax(taxable_amount) {
 return tax
 }
 
-function formatPrice(price) {
+function dollarFormat(price) {
     return '$' + price.toFixed(2);
 }
 
 while (true) {
-    bank_balance = bank_balance - PHONE_PRICE - ACCESSORY_PRICE;
     sub_total = PHONE_PRICE + ACCESSORY_PRICE;
+    bank_balance = bank_balance - sub_total;
     total = sub_total + calculateTax(sub_total);
     if ((total >= SPENDING_THRESHOLD) || (total >= bank_balance)) { break; }
-    document.writeln('purchas phone and accessories at ' + formatPrice(total) + ',bank balance is ' + formatPrice(bank_balance));
+    document.writeln('purchas phone and accessories at ' + dollarFormat(total) + ',bank balance is ' + dolarFormat(bank_balance));
 }
 
 if (total >= SPENDING_THRESHOLD) {
-    document.writeln('Total of ' + formatPrice(total) + ' exceeded threshold of ' + formatPrice(SPENDING_THRESHOLD));
+    document.writeln('Total of ' + dollarFormat(total) + ' exceeded threshold of ' + dollarFormat(SPENDING_THRESHOLD));
 }
 else {
-    document.writeln('Oops, ran out of money. Total of ' +  formatPrice(total) + ' exceeded bank balance of ' + formatPrice(bank_balance));
+    document.writeln('Oops, ran out of money. Total of ' +  dollarFormat(total) + ' exceeded bank balance of ' + dollarFormat(bank_balance + sub_total));
 }
 
