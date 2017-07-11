405bed43a43def3400a6c3695b923f7e13582aca
Paul Stivers <paul.stivers@fidelitylife.com>
2017-07-11 14:52:23 -0700

    first commit

--- functions/function.js
+++ functions/function.js
@@ -3,4 +3,13 @@ function pass_thru(a){
 }
 document.writeln(pass_thru("boo"));
 
+function foo() {
+    return 42;
+}
+
+foo.bar = "hello world";
+
+document.writeln("typeof foo is " + typeof foo);           // "function"
+document.writeln("typeof foo() is " + typeof foo());       // "number"
+document.writeln("typeof foo.bar is " + typeof foo.bar);   // "string"
 
