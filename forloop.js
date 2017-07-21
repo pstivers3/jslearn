81715cbba4dac87a1e4a70a15ae7c91db4477bf6
Paul Stivers <paul.stivers@fidelitylife.com>
2017-07-21 11:03:45 -0700

    first commit

---
+++ forloop.js
@@ -0,0 +1,7 @@
+function boo() {
+    for (var i=0; i<10; i++) {
+        console.log(i);
+    }
+}
+
+boo();
---
+++ scope/undeclared.js
@@ -0,0 +1,6 @@
+function foo(a) {
+    b = a;
+    console.log( a + b );
+}
+
+foo( 2 );
